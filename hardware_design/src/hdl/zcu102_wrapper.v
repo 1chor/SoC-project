c737b4d1757be9f5fd09341681f28e7c9cc1ae22
1chor <e1325347@student.tuwien.ac.at>
2020-08-31 20:52:57 +0200

    Update block design
    Added subblocks for zynq and hdmi

--- hardware_design/src/hdl/zcu102_wrapper.v
+++ hardware_design/src/hdl/zcu102_wrapper.v
@@ -1,8 +1,8 @@
-//Copyright 1986-2019 Xilinx, Inc. All Rights Reserved.
+//Copyright 1986-2018 Xilinx, Inc. All Rights Reserved.
 //--------------------------------------------------------------------------------
-//Tool Version: Vivado v.2019.1 (lin64) Build 2552052 Fri May 24 14:47:09 MDT 2019
-//Date        : Wed Apr 29 20:05:41 2020
-//Host        : soc running 64-bit Ubuntu 18.04.4 LTS
+//Tool Version: Vivado v.2018.1 (lin64) Build 2188600 Wed Apr  4 18:39:19 MDT 2018
+//Date        : Mon Aug 31 19:33:31 2020
+//Host        : soc running 64-bit Ubuntu 18.04.5 LTS
 //Command     : generate_target zcu102_wrapper.bd
 //Design      : zcu102_wrapper
 //Purpose     : IP block netlist
@@ -10,115 +10,115 @@
 `timescale 1 ps / 1 ps
 
 module zcu102_wrapper
-   (hdmi_ctl_iic_scl_io,
-    hdmi_ctl_iic_sda_io,
-    hdmi_tx_clk_n_out,
-    hdmi_tx_clk_p_out,
-    hdmi_tx_dat_n_out,
-    hdmi_tx_dat_p_out,
-    hdmi_tx_ddc_out_scl_io,
-    hdmi_tx_ddc_out_sda_io,
-    hdmi_tx_en_out,
-    hdmi_tx_hb_led,
-    hdmi_tx_hpd_in,
-    hdmi_tx_locked_led,
-    reset,
-    si5324_clk_n_in,
-    si5324_clk_p_in,
-    si5324_lol_in,
-    si5324_rst_out);
-  inout hdmi_ctl_iic_scl_io;
-  inout hdmi_ctl_iic_sda_io;
-  output hdmi_tx_clk_n_out;
-  output hdmi_tx_clk_p_out;
-  output [2:0]hdmi_tx_dat_n_out;
-  output [2:0]hdmi_tx_dat_p_out;
-  inout hdmi_tx_ddc_out_scl_io;
-  inout hdmi_tx_ddc_out_sda_io;
-  output [0:0]hdmi_tx_en_out;
-  output hdmi_tx_hb_led;
-  input hdmi_tx_hpd_in;
-  output hdmi_tx_locked_led;
+   (HDMI_TX_CLK_N_OUT,
+    HDMI_TX_CLK_P_OUT,
+    HDMI_TX_DAT_N_OUT,
+    HDMI_TX_DAT_P_OUT,
+    LED0,
+    LED1,
+    SI5324_LOL_IN,
+    SI5324_RST_OUT,
+    TX_DDC_OUT_scl_io,
+    TX_DDC_OUT_sda_io,
+    TX_EN_OUT,
+    TX_HPD_IN,
+    TX_REFCLK_N_IN,
+    TX_REFCLK_P_IN,
+    fmch_iic_scl_io,
+    fmch_iic_sda_io,
+    reset);
+  output HDMI_TX_CLK_N_OUT;
+  output HDMI_TX_CLK_P_OUT;
+  output [2:0]HDMI_TX_DAT_N_OUT;
+  output [2:0]HDMI_TX_DAT_P_OUT;
+  output LED0;
+  output LED1;
+  input SI5324_LOL_IN;
+  output [0:0]SI5324_RST_OUT;
+  inout TX_DDC_OUT_scl_io;
+  inout TX_DDC_OUT_sda_io;
+  output [0:0]TX_EN_OUT;
+  input TX_HPD_IN;
+  input TX_REFCLK_N_IN;
+  input TX_REFCLK_P_IN;
+  inout fmch_iic_scl_io;
+  inout fmch_iic_sda_io;
   input reset;
-  input si5324_clk_n_in;
-  input si5324_clk_p_in;
-  input si5324_lol_in;
-  output [0:0]si5324_rst_out;
 
-  wire hdmi_ctl_iic_scl_i;
-  wire hdmi_ctl_iic_scl_io;
-  wire hdmi_ctl_iic_scl_o;
-  wire hdmi_ctl_iic_scl_t;
-  wire hdmi_ctl_iic_sda_i;
-  wire hdmi_ctl_iic_sda_io;
-  wire hdmi_ctl_iic_sda_o;
-  wire hdmi_ctl_iic_sda_t;
-  wire hdmi_tx_clk_n_out;
-  wire hdmi_tx_clk_p_out;
-  wire [2:0]hdmi_tx_dat_n_out;
-  wire [2:0]hdmi_tx_dat_p_out;
-  wire hdmi_tx_ddc_out_scl_i;
-  wire hdmi_tx_ddc_out_scl_io;
-  wire hdmi_tx_ddc_out_scl_o;
-  wire hdmi_tx_ddc_out_scl_t;
-  wire hdmi_tx_ddc_out_sda_i;
-  wire hdmi_tx_ddc_out_sda_io;
-  wire hdmi_tx_ddc_out_sda_o;
-  wire hdmi_tx_ddc_out_sda_t;
-  wire [0:0]hdmi_tx_en_out;
-  wire hdmi_tx_hb_led;
-  wire hdmi_tx_hpd_in;
-  wire hdmi_tx_locked_led;
+  wire HDMI_TX_CLK_N_OUT;
+  wire HDMI_TX_CLK_P_OUT;
+  wire [2:0]HDMI_TX_DAT_N_OUT;
+  wire [2:0]HDMI_TX_DAT_P_OUT;
+  wire LED0;
+  wire LED1;
+  wire SI5324_LOL_IN;
+  wire [0:0]SI5324_RST_OUT;
+  wire TX_DDC_OUT_scl_i;
+  wire TX_DDC_OUT_scl_io;
+  wire TX_DDC_OUT_scl_o;
+  wire TX_DDC_OUT_scl_t;
+  wire TX_DDC_OUT_sda_i;
+  wire TX_DDC_OUT_sda_io;
+  wire TX_DDC_OUT_sda_o;
+  wire TX_DDC_OUT_sda_t;
+  wire [0:0]TX_EN_OUT;
+  wire TX_HPD_IN;
+  wire TX_REFCLK_N_IN;
+  wire TX_REFCLK_P_IN;
+  wire fmch_iic_scl_i;
+  wire fmch_iic_scl_io;
+  wire fmch_iic_scl_o;
+  wire fmch_iic_scl_t;
+  wire fmch_iic_sda_i;
+  wire fmch_iic_sda_io;
+  wire fmch_iic_sda_o;
+  wire fmch_iic_sda_t;
   wire reset;
-  wire si5324_clk_n_in;
-  wire si5324_clk_p_in;
-  wire si5324_lol_in;
-  wire [0:0]si5324_rst_out;
 
-  IOBUF hdmi_ctl_iic_scl_iobuf
-       (.I(hdmi_ctl_iic_scl_o),
-        .IO(hdmi_ctl_iic_scl_io),
-        .O(hdmi_ctl_iic_scl_i),
-        .T(hdmi_ctl_iic_scl_t));
-  IOBUF hdmi_ctl_iic_sda_iobuf
-       (.I(hdmi_ctl_iic_sda_o),
-        .IO(hdmi_ctl_iic_sda_io),
-        .O(hdmi_ctl_iic_sda_i),
-        .T(hdmi_ctl_iic_sda_t));
-  IOBUF hdmi_tx_ddc_out_scl_iobuf
-       (.I(hdmi_tx_ddc_out_scl_o),
-        .IO(hdmi_tx_ddc_out_scl_io),
-        .O(hdmi_tx_ddc_out_scl_i),
-        .T(hdmi_tx_ddc_out_scl_t));
-  IOBUF hdmi_tx_ddc_out_sda_iobuf
-       (.I(hdmi_tx_ddc_out_sda_o),
-        .IO(hdmi_tx_ddc_out_sda_io),
-        .O(hdmi_tx_ddc_out_sda_i),
-        .T(hdmi_tx_ddc_out_sda_t));
+  IOBUF TX_DDC_OUT_scl_iobuf
+       (.I(TX_DDC_OUT_scl_o),
+        .IO(TX_DDC_OUT_scl_io),
+        .O(TX_DDC_OUT_scl_i),
+        .T(TX_DDC_OUT_scl_t));
+  IOBUF TX_DDC_OUT_sda_iobuf
+       (.I(TX_DDC_OUT_sda_o),
+        .IO(TX_DDC_OUT_sda_io),
+        .O(TX_DDC_OUT_sda_i),
+        .T(TX_DDC_OUT_sda_t));
+  IOBUF fmch_iic_scl_iobuf
+       (.I(fmch_iic_scl_o),
+        .IO(fmch_iic_scl_io),
+        .O(fmch_iic_scl_i),
+        .T(fmch_iic_scl_t));
+  IOBUF fmch_iic_sda_iobuf
+       (.I(fmch_iic_sda_o),
+        .IO(fmch_iic_sda_io),
+        .O(fmch_iic_sda_i),
+        .T(fmch_iic_sda_t));
   zcu102 zcu102_i
-       (.hdmi_ctl_iic_scl_i(hdmi_ctl_iic_scl_i),
-        .hdmi_ctl_iic_scl_o(hdmi_ctl_iic_scl_o),
-        .hdmi_ctl_iic_scl_t(hdmi_ctl_iic_scl_t),
-        .hdmi_ctl_iic_sda_i(hdmi_ctl_iic_sda_i),
-        .hdmi_ctl_iic_sda_o(hdmi_ctl_iic_sda_o),
-        .hdmi_ctl_iic_sda_t(hdmi_ctl_iic_sda_t),
-        .hdmi_tx_clk_n_out(hdmi_tx_clk_n_out),
-        .hdmi_tx_clk_p_out(hdmi_tx_clk_p_out),
-        .hdmi_tx_dat_n_out(hdmi_tx_dat_n_out),
-        .hdmi_tx_dat_p_out(hdmi_tx_dat_p_out),
-        .hdmi_tx_ddc_out_scl_i(hdmi_tx_ddc_out_scl_i),
-        .hdmi_tx_ddc_out_scl_o(hdmi_tx_ddc_out_scl_o),
-        .hdmi_tx_ddc_out_scl_t(hdmi_tx_ddc_out_scl_t),
-        .hdmi_tx_ddc_out_sda_i(hdmi_tx_ddc_out_sda_i),
-        .hdmi_tx_ddc_out_sda_o(hdmi_tx_ddc_out_sda_o),
-        .hdmi_tx_ddc_out_sda_t(hdmi_tx_ddc_out_sda_t),
-        .hdmi_tx_en_out(hdmi_tx_en_out),
-        .hdmi_tx_hb_led(hdmi_tx_hb_led),
-        .hdmi_tx_hpd_in(hdmi_tx_hpd_in),
-        .hdmi_tx_locked_led(hdmi_tx_locked_led),
-        .reset(reset),
-        .si5324_clk_n_in(si5324_clk_n_in),
-        .si5324_clk_p_in(si5324_clk_p_in),
-        .si5324_lol_in(si5324_lol_in),
-        .si5324_rst_out(si5324_rst_out));
+       (.HDMI_TX_CLK_N_OUT(HDMI_TX_CLK_N_OUT),
+        .HDMI_TX_CLK_P_OUT(HDMI_TX_CLK_P_OUT),
+        .HDMI_TX_DAT_N_OUT(HDMI_TX_DAT_N_OUT),
+        .HDMI_TX_DAT_P_OUT(HDMI_TX_DAT_P_OUT),
+        .LED0(LED0),
+        .LED1(LED1),
+        .SI5324_LOL_IN(SI5324_LOL_IN),
+        .SI5324_RST_OUT(SI5324_RST_OUT),
+        .TX_DDC_OUT_scl_i(TX_DDC_OUT_scl_i),
+        .TX_DDC_OUT_scl_o(TX_DDC_OUT_scl_o),
+        .TX_DDC_OUT_scl_t(TX_DDC_OUT_scl_t),
+        .TX_DDC_OUT_sda_i(TX_DDC_OUT_sda_i),
+        .TX_DDC_OUT_sda_o(TX_DDC_OUT_sda_o),
+        .TX_DDC_OUT_sda_t(TX_DDC_OUT_sda_t),
+        .TX_EN_OUT(TX_EN_OUT),
+        .TX_HPD_IN(TX_HPD_IN),
+        .TX_REFCLK_N_IN(TX_REFCLK_N_IN),
+        .TX_REFCLK_P_IN(TX_REFCLK_P_IN),
+        .fmch_iic_scl_i(fmch_iic_scl_i),
+        .fmch_iic_scl_o(fmch_iic_scl_o),
+        .fmch_iic_scl_t(fmch_iic_scl_t),
+        .fmch_iic_sda_i(fmch_iic_sda_i),
+        .fmch_iic_sda_o(fmch_iic_sda_o),
+        .fmch_iic_sda_t(fmch_iic_sda_t),
+        .reset(reset));
 endmodule
