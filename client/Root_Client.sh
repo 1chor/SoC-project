78d9cc47e4ada2731bfd28d80f9bf292b09e35b0
1chor <e1325347@student.tuwien.ac.at>
2020-11-01 15:30:14 +0100

    added support for filter function

--- client/Root_Client.sh
+++ client/Root_Client.sh
@@ -3,20 +3,31 @@
 Filepath=/storage/emulated/0/SoC
 
 cd $Filepath
+rm *
+echo > filtered.bin
 
 while [ 1 ]; do
 	sleep 10
-	
+		
 	# control for filter
 	if [ -f "$Filepath/filter.txt" ]; then
 		echo "filter.txt exists." > /dev/kmsg
 		
+		rm $Filepath/fBitmap.bin
+		
 		# echo bitmap to filter kernel module
 		echo "$( < filter.txt )" > /proc/simple_filters
 		
+		# wait for filter to complete
+		sleep 0.05
+		
+		# rename filtered bitmap for app
+		cp filtered.bin fBitmap.bin 
+		
+		rm "$( < filter.txt )"
 		rm $Filepath/filter.txt
 	fi
-	
+		
 	# control for blake2b
 	if [ -f "$Filepath/blake2b.txt" ]; then
 		echo "blake2b.txt exists." > /dev/kmsg
@@ -29,7 +40,7 @@ while [ 1 ]; do
 		echo "$( < blake2b.txt )" > /proc/blake2b
 		
 		# wait for hash to complete
-		sleep 0.05
+		sleep 0.1
 		
 		# read blake2b kernel module
 		cat /proc/blake2b > $Filepath/hash.txt
