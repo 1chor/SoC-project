5d37140b1e6880fd964b68cd0ee740adf0bdc312
1chor <e1325347@student.tuwien.ac.at>
2020-11-01 20:15:33 +0100

    Update hash and partial function

--- client/Root_Client.sh
+++ client/Root_Client.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
 Filepath=/storage/emulated/0/SoC
+emptyhash="00000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000"
 
 if [ ! -d $Filepath ]; then
 	mkdir -p $Filepath
@@ -36,6 +37,7 @@ while [ 1 ]; do
 	if [ -f "$Filepath/blake2b.txt" ]; then
 		echo "blake2b.txt exists." > /dev/kmsg
 		rm $Filepath/hash.txt
+		rm $Filepath/tmp.txt
 		
 		# cat bitstream to null device, otherwise the kernel module will fail 
 		cat "$( < blake2b.txt )" > /dev/null
@@ -47,7 +49,23 @@ while [ 1 ]; do
 		sleep 0.1
 		
 		# read blake2b kernel module
-		cat /proc/blake2b > $Filepath/hash.txt
+		cat /proc/blake2b > $Filepath/tmp.txt
+		
+		# check if hash is written
+		isInFile=$(cat tmp.txt | grep -c "$emptyhash")
+
+		while [ $isInFile -ne 0 ]; do
+			# wait for hash to complete
+			sleep 0.05
+			
+			# read blake2b kernel module
+			cat /proc/blake2b > $Filepath/tmp.txt
+		
+			# check if hash is written
+			isInFile=$(cat tmp.txt | grep -c "$emptyhash")
+		done
+			
+		cp tmp.txt hash.txt 
 		
 		rm $Filepath/blake2b.txt
 	fi
@@ -56,19 +74,25 @@ while [ 1 ]; do
 	if [ -f "$Filepath/partial.txt" ]; then
 		echo "partial.txt exists." > /dev/kmsg
 		
+		# set variables
+		bitpath="$( < partial.txt )"
+		echo $bitpath > /dev/null
+		bit=${bitpath##*/}
+		echo $bit > /dev/null
+		
 		# set flags for partial bitstream
 		echo 1 > /sys/class/fpga_manager/fpga0/flags
 		
 		# load partial bitstream
-		cp "$( < partial.txt )" /lib/firmware
-		echo "$( < partial.txt )" /sys/class/fpga_manager/fpga0/firmware
+		cp $bitpath /lib/firmware
+		echo $bit > /sys/class/fpga_manager/fpga0/firmware
 		
 		# control LEDs dependent on the filter reconfigured
-		if [ "$( < partial.txt )" = "blue_filter.bin" ]; then 
+		if [ $bit = "blue_filter.bin" ]; then 
 			echo 0x03 > /proc/myled
-		elif [ "$( < partial.txt )" = "green_filter.bin" ]; then 
+		elif [ $bit = "green_filter.bin" ]; then 
 			echo 0x18 > /proc/myled
-		elif [ "$( < partial.txt )" = "red_filter.bin" ]; then 
+		elif [ $bit = "red_filter.bin" ]; then 
 			echo 0C0 > /proc/myled
 		else
 			echo 0x00 > /proc/myled
